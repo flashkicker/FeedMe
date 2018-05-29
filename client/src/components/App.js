c91eeb43db93058027b6da73931ace5f9d9842d3
Ujjval <ujjvalkumaria@gmail.com>
2018-05-29 03:28:20 -0700

    materializecss setup and basic header in place

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from "react"
+import { BrowserRouter, Route } from "react-router-dom"
+import Header from './Header'
 
-const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Route path="/" component={Landing} />
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+                    <Header />
+					<Route exact path="/" component={Landing} /> 
+					<Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
                 </div>
-            </BrowserRouter>
-        </div>
-    )
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App
 
 // this file is responsible for the initial view layer setup
+// exact can be written as exact={true}; jsx thingy
