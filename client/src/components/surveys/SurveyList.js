e56e3f65111ae4e92c73eec5924bd1bd973dc445
Ujjval <ujjvalkumaria@gmail.com>
2019-03-18 21:48:49 -0700

    SurveyList card view implemented

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -4,12 +4,34 @@ import { connect } from "react-redux"
 import { fetchSurveys } from "../../actions"
 
 class SurveyList extends Component {
-    componentDidMount() {
-        this.props.fetchSurveys()
-    }
+	componentDidMount() {
+		this.props.fetchSurveys()
+	}
+
+	renderSurveys() {
+		return this.props.surveys.reverse().map(survey => {
+			const { _id, title, body, dateSent, yes, no } = survey
+
+			return (
+				<div key={_id} className="card darken-1">
+					<div className="card-content">
+						<span className="card-title">{title}</span>
+						<p>{body}</p>
+						<p className="right">
+							Sent On: {new Date(dateSent).toLocaleDateString()}
+						</p>
+						<div className="card-action">
+							<a>Yes: {yes}</a>
+							<a>No: {no}</a>
+						</div>
+					</div>
+				</div>
+			)
+		})
+	}
 
 	render() {
-		return <div>SurveyList</div>
+		return <div>{this.renderSurveys()}</div>
 	}
 }
 
