67d253d620007f1ce5c79c2cfacfa0e125097d19
Ujjval <ujjvalkumaria@gmail.com>
2019-03-18 21:28:00 -0700

    surveys wired up to redux

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { FETCH_USER } from "./types"
+import { FETCH_USER, FETCH_SURVEYS } from "./types"
 
 export const fetchUser = () => {
 	return async dispatch => {
@@ -23,5 +23,13 @@ export const submitSurvey = (values, history) => {
 
 		history.push("/surveys")
 		dispatch({ type: FETCH_USER, payload: res.data })
+	}
+}
+
+export const fetchSurveys = () => {
+	return async dispatch => {
+		const res = await axios.get("/api/surveys")
+
+		dispatch({ type: FETCH_SURVEYS, payload: res.data })
 	}
 }
